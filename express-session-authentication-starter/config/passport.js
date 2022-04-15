2dfb02d174f6568b5a58c0917f6205c1f908e2ab
Aradhya Patro <aradhyapatro14@gmail.com>
2022-04-15 21:59:44 +0530

    the passport session done

--- express-session-authentication-starter/config/passport.js
+++ express-session-authentication-starter/config/passport.js
@@ -1,6 +1,50 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const connection = require('./database');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const connection = require("./database");
+const validPassword = require("../lib/passwordUtils").validPassword;
 const User = connection.models.User;
 
-// TODO: passport.use();
+const customFields = {
+  username: "aradhya",
+  password: "123456",
+};
+
+const verifyCallBack = (username, password, done) => {
+  User.findOne({ username: username })
+    .then((user) => {
+      if (!user) {
+        return done(null, false);
+      }
+
+      const hash = user.hash;
+      const salt = user.salt;
+
+      const isValid = validPassword(password, hash, salt);
+      if (isValid) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    })
+    .catch((err) => {
+      done(err);
+    });
+};
+
+const Strategy = new LocalStrategy(customFields, verifyCallBack);
+
+passport.use(Strategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((userid, done) => {
+  User.findById(userid)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
--- express-session-authentication-starter/lib/passwordUtils.js
+++ express-session-authentication-starter/lib/passwordUtils.js
@@ -1,8 +1,20 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-// TODO
-function validPassword(password, hash, salt) {}
-function genPassword(password) {}
+function validPassword(password, hash, salt) {
+  const conHash = crypto
+    .pbkdf2Sync(password, salt, 10000, 60, "sha512")
+    .toString("hex");
+  return hash === conHash;
+}
+
+function genPassword(password) {
+  let salt = crypto.randomBytes(30).toString("hex");
+  let genHash = crypto
+    .pbkdf2Sync(password, salt, 10000, 60, "sha512")
+    .toString("hex");
+
+  return { salt: salt, hash: genHash };
+}
 
 module.exports.validPassword = validPassword;
 module.exports.genPassword = genPassword;
